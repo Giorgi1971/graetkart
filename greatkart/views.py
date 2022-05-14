4f8f0a8c61e7765d9b70d66838be83537a8dce76
Giorgi1971 <giooorgi7@gmail.com>
2022-05-14 10:00:35 +0400

    commited for pull in mac

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,11 +1,8 @@
-
-import imp
-from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import *
 
 
 def home(request):
     products = Product.objects.all().filter(is_avaliable=True)
-    context = {'products':products}
+    context = {'products': products}
     return render(request, 'home.html', context)
